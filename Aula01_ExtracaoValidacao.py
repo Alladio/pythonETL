e7f3a8cc548fc91291fbde683574cfa80e478d66
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-27 22:53:42 -0300

    extracao e validacao

--- Aula01_ExtracaoValidacao.py
+++ Aula01_ExtracaoValidacao.py
@@ -1,7 +1,8 @@
 #EXTRAÇÃO E VALIDAÇÃO
 
 import pandas as pd
-df = pd.read_csv("ocorrencia.csv", parse_dates=['ocorrencia_dia'])
+import pandera as pd (pip install pandera --user)
+df = pd.read_csv("ocorrencia.csv", parse_dates=['ocorrencia_dia'], dayfirst=True)
 df.head(10)
 df.dtypes
 df.ocorrencia_dia.dt.month
