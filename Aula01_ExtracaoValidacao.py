156438ce4eb875fa99a311d5de5576a3128d1491
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-28 20:24:53 -0300

    Update Aula01_ExtracaoValidacao.py

--- Aula01_ExtracaoValidacao.py
+++ Aula01_ExtracaoValidacao.py
@@ -1,7 +1,7 @@
 #EXTRAÇÃO E VALIDAÇÃO
 
 import pandas as pd
-import pandera as pd (pip install pandera --user)
+import pandera as pd #(pip install pandera --user)
 df = pd.read_csv("ocorrencia.csv", parse_dates=['ocorrencia_dia'], dayfirst=True)
 df.head(10)
 df.dtypes
